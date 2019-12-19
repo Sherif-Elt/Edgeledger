c987649bc4390d7d88d630b2aa62389e24b6ddfc
Sherif <sherif@iMac-van-Sherif.local>
2019-12-19 10:51:12 +0100

    fd

--- process.php
+++ process.php
@@ -1,12 +1,24 @@
-<?php 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "support@sherif-elt.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    $to = "support@sherif-elt.com";
+    $body = 'Here your email body text';
+    $subject = 'NEW Subscriber';
+    $headers = 'MIME-Version: 1.0' . "\r\n"; 
+    $headers .= "From: $email \r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    if($_POST['submit']){
+        if(mail($to, $subject, $body, $headers)){
+             echo "mail send"; // change this later to your header location
+        }
+        else{
+            echo 'error sending mail';
+        }
+    }
+    else{
+        echo 'no post data';
+    }
 ?>
